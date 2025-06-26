21db693b18204f86d1c0da34ac6fd0d880beca28
Hitezverma12 <hitezverma12@yahoo.com>
2025-06-26 17:27:50 +1000

    Add classes for India and USA with methods for capital, language, and type

---
+++ country.py
@@ -0,0 +1,19 @@
+class india():
+    def capital(self):
+         print("new delhi is the capital of india")
+    
+    def language(self):
+        print("Hindi is the official language of India")    
+
+    def type (self):
+        print("India is a democratic country")
+
+class usa(): 
+    def capital(self):
+        print("Washington D.C. is the capital of USA")
+    
+    def language(self):
+        print("English is the official language of USA")    
+
+    def type (self):
+        print("USA is a developed country")
